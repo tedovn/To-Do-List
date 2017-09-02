ba1986daeaa84aa33f25bd55288a500b4f33f7d3
Teodor Dobrev <teodor.dobrev@1stonlinesolutions.com>
2017-09-02 11:24:41 +0300

    for djekito

---
+++ djekito/Java Solution/src/main/java/shoppinglist/bindingModel/ProductBindingModel.java
@@ -0,0 +1,40 @@
+package shoppinglist.bindingModel;
+
+public class ProductBindingModel {
+    private String name;
+    private int quantity;
+    private String status;
+    private int priority;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
